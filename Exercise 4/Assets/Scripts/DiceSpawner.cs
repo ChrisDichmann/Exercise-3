dd1989630dba8e24172256849b73cee231093fe2
Chris Dichmann <christoph3rs0n@gmail.com>
2021-04-22 13:19:58 -0700

    Dice Spawner
    Doing work on the dice spawn

---
+++ Exercise 4/Assets/Scripts/DiceSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceSpawner : MonoBehaviour
+{
+    public GameObject invisibleSpawn;
+    private GameObject dicePrefab;
+    public float positionX;
+    public int diceSelector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positionX = invisibleSpawn.transform.position.x;
+        positionX = 80;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //DiceSpawn();
+        RandomizeDice();
+    }
+
+   /* public void DiceSpawn()
+    {
+        for (positionX = 80; positionX < 960; positionX += 160)
+        {
+            Instantiate(dicePrefab, gameObject.transform.position, gameObject.transform.rotation);
+        }
+
+    } */
+
+    public void RandomizeDice()
+    {
+        System.Random r = new System.Random();
+        int diceSelector = r.Next(1, 36);
+
+        string diceVariable = string.Format("Dice{diceSelector}");
+        Debug.Log(diceVariable);
+    }
+}

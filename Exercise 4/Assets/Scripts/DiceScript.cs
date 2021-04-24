51d52a55c84edd367f7c8c289734d4da2049e858
Chris Dichmann <christoph3rs0n@gmail.com>
2021-04-24 08:16:26 -0700

    Movement in Spanish
    Updated the movement of the Dice objects so they can swap locations with limited ability

--- Exercise 4/Assets/Scripts/DiceScript.cs
+++ Exercise 4/Assets/Scripts/DiceScript.cs
@@ -11,11 +11,17 @@ public class DiceScript : MonoBehaviour
     public int Value;
     public int Color;
 
+    private bool Switch;
+
+    public GameObject heldDice;
+
     private SpriteRenderer spriteRender;
 
     // Start is called before the first frame update
     void Start()
     {
+        Switch = false;
+
         Value = Random.Range(0, 5); //sets the integer variable behind the number on the dice to a random of 0-5
         Color = Random.Range(0, 5); //same as above, but for the variable behind the color on the dice
 
@@ -28,6 +34,44 @@ public class DiceScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+                if (Switch == false)
+                {
+                    hit.transform.position = new Vector3(0f, -3.5f, 0f);
+                    Switch = true;
+                }
+
+                else if (Switch == true)
+                {
+                    float clickedValue = hit.transform.GetComponent<DiceScript>().Value;
+                    Debug.Log(clickedValue);
+                    GameObject heldDice = GameObject.FindGameObjectWithTag("Held");
+                    float heldValue = heldDice.GetComponent<DiceScript>().Value;
+
+                    if (heldValue != 5 && clickedValue == (heldValue - 1))
+                    {
+
+                    }
+
+                    else if (heldValue == 5 && clickedValue == 0)
+                    {
+
+                    }
+
+                    else if (heldDice.layer == 2)
+                    {
+                        hit.transform.position = heldDice.transform.position;
+                        Destroy(heldDice);
+                    }
+                }
+            }
+        }
     }
 }
--- Exercise 4/Assets/Scripts/DiceSpawner.cs
+++ Exercise 4/Assets/Scripts/DiceSpawner.cs
@@ -18,24 +18,26 @@ public class DiceSpawner : MonoBehaviour
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0)); //screenBounds now holds the relative positional information for the screen of the game
         fiveChunk = (screenBounds.x / 3); //fiveChunk, the main variable that splits up the placement of the dice, is set to 1/5 of the screen
         positionX = -2 * fiveChunk; //position variable is set to fiveChunk
-        positionY = screenBounds.y;
+        positionY = screenBounds.y / 3 + 1;
         transform.position = new Vector2(positionX, positionY); //x position of the spawner object is SHOCKINGLY given to position x
         DiceSpawn(); //instigates the DiceSpawn function
     }
 
     // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public void DiceSpawn()
     {
         for (positionX = -2 * fiveChunk; positionX < screenBounds.x; positionX += fiveChunk)
         {
             Instantiate(dicePrefab, new Vector2(positionX, positionY), gameObject.transform.rotation);
-            Debug.Log(fiveChunk);
         }
 
+        positionX = -2 * fiveChunk;
+        positionY = screenBounds.y / 3 - 2;
+
+        for (positionX = -2 * fiveChunk; positionX < screenBounds.x; positionX += fiveChunk)
+        {
+            Instantiate(dicePrefab, new Vector2(positionX, positionY), gameObject.transform.rotation);
+        }
     }
 }

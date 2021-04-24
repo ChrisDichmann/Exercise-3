03906acbce7bc70f6c2ee8b5adcc9baf6d0a7fc9
Chris Dichmann <christoph3rs0n@gmail.com>
2021-04-24 05:27:14 -0700

    Updated Dice Script
    Scrapped old brute-force Dice randomization, implemented new code and updated sprites.

---
+++ Exercise 4/Assets/Scripts/DiceScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceScript : MonoBehaviour
+{
+
+    public Sprite[] sprites;
+    public Color[] colors;
+
+    public int Value;
+    public int Color;
+
+    private SpriteRenderer spriteRender;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Value = Random.Range(1, 6); //sets the integer variable behind the number on the dice to a random of 1-6
+        Color = Random.Range(1, 6); //same as above, but for the variable behind the color on the dice
+
+        spriteRender = GetComponent<SpriteRenderer>(); //grabs the sprite renderer for the container object: in this case, the Dice object
+
+        spriteRender.sprite = sprites[Value]; //sets the sprite itself to one that corresponds with the Value variable on the array list
+        spriteRender.color = colors[Color]; //same as above for color: these are dual systems.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
